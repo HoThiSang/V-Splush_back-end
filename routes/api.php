6ff0ff1c22b91c356dc511e8d3c3b9eec371b803
Linh <nguyenthilinhqni2020@gmail.com>
2024-04-25 09:58:26 +0700

    feat: show all comment for admin refer to ST-54

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminCommentController;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/show-all-comments',[AdminCommentController::class,'index']);
+Route::post('/add-comment',[AdminCommentController::class,'store']);
+Route::put('update-comment/{id}',[AdminCommentController::class,'update']);
+Route::delete('delete-comment/{id}',[AdminCommentController::class,'destroy']);
+Route::get('/show-detail-comment/{id}',[AdminCommentController::class,'show']);
